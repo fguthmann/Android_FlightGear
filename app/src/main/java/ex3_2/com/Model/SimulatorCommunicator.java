152e84c422412952e828fa3c884b1c3462eb4398
IdoAvTr <81075460+IdoAvTr@users.noreply.github.com>
2021-06-26 22:02:58 +0300

    add content to the model

--- app/src/main/java/ex3_2/com/Model/SimulatorCommunicator.java
+++ app/src/main/java/ex3_2/com/Model/SimulatorCommunicator.java
@@ -1,4 +1,68 @@
 package ex3_2.com.Model;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class SimulatorCommunicator {
+    private Client client;
+    public SimulatorCommunicator(){
+        client = new Client();
+        client.map = new LinkedHashMap<>();
+    }
+    public void AddAttribute(String name, String initialValue){
+        client.map.put(name, Float.valueOf(initialValue).floatValue());
+    }
+    public void SetAttribute(String theAttribute, float value){
+        client.map.put(theAttribute, Float.valueOf(value).floatValue());
+    }
+    private void StartCommunication(String address, String port)
+            throws IOException, UnknownHostException {
+        InetAddress adr = InetAddress.getByName(address);
+        client.StartFlight(adr, Integer.parseInt(port));
+    }
+    public String StartFlight(String address, String port) {
+        try {
+            StartCommunication(address, port);
+        }catch (UnknownHostException e){
+            return "IP address not valid.";
+        }
+        catch (IOException e){
+            return "Connection Error.";
+        }
+        return null;
+    }
+    public void endFlight(){
+        client.shouldFly = false;
+    }
+
+    private class Client{
+        public Map<String, Float> map;
+        public boolean shouldFly = true;
+
+        private void Communicate(PrintWriter writer){
+            while (shouldFly){
+                for (String name: map.keySet()) {
+                    String s = "set /controls/flight/";
+                    s += name + " " + map.get(name).toString();
+                    writer.println(s);
+                }
+            }
+        }
+        public void StartFlight(InetAddress address, int port)
+                throws IOException, UnknownHostException {
+            Socket s = new Socket(address, port);
+            OutputStream output = s.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            Communicate(writer);
+            writer.close();
+            output.close();
+            s.close();
+        }
+    }
 }
